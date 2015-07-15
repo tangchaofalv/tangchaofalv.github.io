c87dd6455298efccee430d8709f71ccad20363b7
tangchaofalv <tangchaofalv@gmail.com>
2015-07-15 17:20:25 +0800

    添加图片

---
+++ firstWeb/lib/myDatas.js
@@ -0,0 +1,18 @@
+/*静态数据存储*/
+
+/*我的图片*/
+var data_myPhotos = [
+	{
+		'id':'indexRollImg',
+		'url':'./images/photos/indexRollImg/',
+		'title':'',
+		'type':'',
+		'photos':[
+			{'imgName':'first.jpg','title':'','description':'','width':'','height':''},
+			{'imgName':'second.jpg','title':'','description':'','width':'','height':''},
+			{'imgName':'third.jpg','title':'','description':'','width':'','height':''},
+			{'imgName':'fourth.jpg','title':'','description':'','width':'','height':''},
+			{'imgName':'fifth.jpg','title':'','description':'','width':'','height':''}
+		]
+	}
+];
